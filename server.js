6961db4ab5408a9da1f350c400afaed24d6e98b5
parn.pichaon <parn.pichaon@gmail.com>
2019-02-02 15:20:32 +0700

    copy r2

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.post('/webhook', middleware(config), (req, res) => {  //req = request
   if (event.type === 'message') {
     const message = event.message;
     console.log(message)
-    client.replyMessage(event.replyToken, 
+    /* client.replyMessage(event.replyToken, 
       {
         "type": "template",
         "altText": "This is a buttons template",
@@ -60,12 +60,68 @@ app.post('/webhook', middleware(config), (req, res) => {  //req = request
       })
   }
 
-  res.send('webhook success')//
+/*   res.send('webhook success')//
   
+
+
+
+app.set('port', (process.env.PORT || 4000)) */
+
+
+client.replyMessage(event.replyToken, {
+  "type": "template",
+  "altText": "this is a carousel template",
+  "template": {
+      "type": "carousel",
+      "columns": [
+          {
+            "thumbnailImageUrl": "https://vignette.wikia.nocookie.net/line/images/b/bb/2015-brown.png/revision/latest?cb=20150808131630",
+            "imageBackgroundColor": "#FFFFFF",
+            "title": "this is menu",
+            "text": "description",
+            "actions": [
+                {  
+                    "type":"cameraRoll",
+                    "label":"Camera roll"
+                },
+                {  
+                  "type":"location",
+                  "label":"Location"
+               }
+            ]
+          },
+          {
+            "thumbnailImageUrl": "https://c.76.my/Malaysia/line-brown-bear-cute-pencil-case-ubiyo-1802-02-Ubiyo@6.jpg",
+            "imageBackgroundColor": "#000000",
+            "title": "this is menu",
+            "text": "description",
+            "actions": [
+              {
+                "type":"datetimepicker",
+                "label":"Select date",
+                "data":"storeId=12345",
+                "mode":"datetime",
+                "initial":"2017-12-25t00:00",
+                "max":"2018-01-24t23:59",
+                "min":"2017-12-25t00:00"
+              },
+              {  
+                "type":"camera",
+                "label":"Camera"
+             }
+          ]
+          }
+      ],
+      "imageAspectRatio": "rectangle",
+      "imageSize": "cover"
+  }
+})
+}
 })
 
-
 app.set('port', (process.env.PORT || 4000))
+
+
 
 app.listen(app.get('port'), function () {
   console.log('run at port', app.get('port'))
